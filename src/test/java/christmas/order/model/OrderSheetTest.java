afd67bc6757b384e3908c09e706979b7660a734b
Potato-Y <projectpotatoy@gmail.com>
2023-11-11 01:03:27 +0900

    test: 잘못된 테스트 코드 수정
    - 테스트 대상이 잘못된 문제 수정

--- src/test/java/christmas/order/model/OrderSheetTest.java
+++ src/test/java/christmas/order/model/OrderSheetTest.java
@@ -1,36 +1,28 @@
 package christmas.order.model;
 
 import static christmas.utils.TestUtil.ERROR_PREFACE;
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class OrderSheetTest {
 
-    @DisplayName("수량이 정상적으로 저장")
-    @Test
-    void saveQuantitySuccess() {
-        // given
-        int quantity = 10;
-
-        // when
-        OrderQuantity orderQuantity = new OrderQuantity(String.valueOf(quantity));
-
-        // then
-        assertThat(orderQuantity.getQuantity())
-                .isEqualTo(quantity);
+    @DisplayName("정상적인 주문은 저장")
+    @ValueSource(strings = {"양송이수프-2,바비큐립-1,아이스크림-1", "타파스-1,레드와인-1"})
+    @ParameterizedTest
+    void createOrderSheet(String input) {
+        assertThatNoException().isThrownBy(() ->
+                new OrderSheet(input));
     }
 
-    @DisplayName("수량 범위를 넘어서면 예외가 발생")
-    @ValueSource(strings = {"21", "0", "-10", "100"})
+    @DisplayName("정상적이지 않은 주문은 예외 발생")
+    @ValueSource(strings = {"바비큐립-1,메뉴-1", "바베큐립-1,", "아무말", "제로콜라-1"})
     @ParameterizedTest
-    void quantityThrow(String input) {
-        assertThatThrownBy(() ->
-                new OrderQuantity(input))
+    void createOrderSheetThrow(String input) {
+        assertThatThrownBy(() -> new OrderSheet(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ERROR_PREFACE);
     }

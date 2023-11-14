9bc5431cf9cb9e92a1ae1f4f36c8283c9ba63702
Potato-Y <projectpotatoy@gmail.com>
2023-11-14 22:40:42 +0900

    fix: 빈 줄이 출력되는 문제 수정

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -6,7 +6,7 @@ import christmas.view.util.PrintUtil;
 public class InputView {
 
     public static String readOrder() {
-        PrintUtil.firstPrint("주문하실 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        PrintUtil.println("주문하실 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
         return Console.readLine();
     }
 

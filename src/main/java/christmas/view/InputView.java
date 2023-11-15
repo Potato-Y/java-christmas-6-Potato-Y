07a44e8dfa99f4769282d4a6ce34ee507e41f357
Potato-Y <projectpotatoy@gmail.com>
2023-11-15 22:47:27 +0900

    fix: 첫 시작 시 상단에 공백이 생기는 문제 수정

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -11,7 +11,7 @@ public class InputView {
     }
 
     public static String readDayOfMonth() {
-        PrintUtil.firstPrint("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+        PrintUtil.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
         PrintUtil.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
 
         return Console.readLine();
